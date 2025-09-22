88edfb9765136a753bd837f9803b55d877ef9f72
ShivaniGanatra <shivaniganatra@live.com>
2025-09-22 21:24:06 +0100

    Found way to transform letters to morse

--- src/main/scala/MorseCode.scala
+++ src/main/scala/MorseCode.scala
@@ -1,4 +1,4 @@
-class MorseCode {
+object MorseCode extends App{
   val morseCode: Map[Char, String] = Map(
     'A' -> ".-", 'B' -> "-...", 'C' -> "-.-.", 'D' -> "-..",
     'E' -> ".", 'F' -> "..-.", 'G' -> "--.", 'H' -> "....",
@@ -8,4 +8,14 @@ class MorseCode {
     'U' -> "..-", 'V' -> "...-", 'W' -> ".--", 'X' -> "-..-",
     'Y' -> "-.--", 'Z' -> "--..", ' ' -> "/"
   )
+  //val new_collection = collection.map(x => x * x )
+  def letterToMorse(a: String): String = {
+    val b = a.toUpperCase.toList
+    val c = b.map(x=>morseCode(x))
+    c.toString()
+
+
+  }
+
+  println(letterToMorse("shivani"))
 }
