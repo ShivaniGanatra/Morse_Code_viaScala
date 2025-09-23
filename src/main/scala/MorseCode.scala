6dccbca09e8a3e0b1d04295fa19577cb81b18516
ShivaniGanatra <shivaniganatra@live.com>
2025-09-23 22:00:53 +0100

    Moved conversion logic to translator

--- src/main/scala/MorseCode.scala
+++ src/main/scala/MorseCode.scala
@@ -1,4 +1,4 @@
-object MorseCode extends App{
+class MorseCode extends App{
   val morseCode: Map[Char, String] = Map(
     'A' -> ".-", 'B' -> "-...", 'C' -> "-.-.", 'D' -> "-..",
     'E' -> ".", 'F' -> "..-.", 'G' -> "--.", 'H' -> "....",
@@ -8,24 +8,5 @@ object MorseCode extends App{
     'U' -> "..-", 'V' -> "...-", 'W' -> ".--", 'X' -> "-..-",
     'Y' -> "-.--", 'Z' -> "--..", ' ' -> "/"
   )
-
   val reversedMorse = morseCode.map(x => x.swap) //reversed key value to value key
-
-  def letterToMorse(letterString: String): String = {
-    val uppercaseList = letterString.toUpperCase.toList
-    val morseCodeList = uppercaseList.map(x=>morseCode(x))
-    morseCodeList.mkString(" ") //make string
-  }
-
-  def morseToLetter(morseString: String): String = {
-    val morseList = morseString.trim.split(" ").toList //added trim in case of unnecessary spaces in front of and at end of word
-    val letterList = morseList.map(x => reversedMorse(x))
-    letterList.mkString //make string
-  }
-
-  println("shivani in morse code is " + letterToMorse("shivani"))
-
-  println("morse to words is " + morseToLetter("    - .... .. ... / .. ... / ... -.-. .- .-.. .-   "))
-//morse to words is THIS IS SCALA
- //when applying use try catch in main file
 }
--- src/main/scala/Translator.scala
+++ src/main/scala/Translator.scala
@@ -1,3 +1,14 @@
-class Translator {
+object Translator extends App{
+  val morseCodeObject = new MorseCode
+  def letterToMorse(letterString: String): String = {
+    val uppercaseList = letterString.toUpperCase.toList
+    val morseCodeList = uppercaseList.map(x => morseCodeObject.morseCode(x))
+    morseCodeList.mkString(" ") //make string
+  }
 
+  def morseToLetter(morseString: String): String = {
+    val morseList = morseString.trim.split(" ").toList //added trim in case of unnecessary spaces in front of and at end of word
+    val letterList = morseList.map(x => morseCodeObject.reversedMorse(x))
+    letterList.mkString //make string
+  }
 }
