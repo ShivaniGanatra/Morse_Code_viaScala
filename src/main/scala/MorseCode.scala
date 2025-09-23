a6a809b2462292c6812116f79517a3ebb4834a0a
ShivaniGanatra <shivaniganatra@live.com>
2025-09-23 21:52:48 +0100

    Found way to change morse to words

--- src/main/scala/MorseCode.scala
+++ src/main/scala/MorseCode.scala
@@ -8,14 +8,24 @@ object MorseCode extends App{
     'U' -> "..-", 'V' -> "...-", 'W' -> ".--", 'X' -> "-..-",
     'Y' -> "-.--", 'Z' -> "--..", ' ' -> "/"
   )
-  //val new_collection = collection.map(x => x * x )
-  def letterToMorse(a: String): String = {
-    val b = a.toUpperCase.toList
-    val c = b.map(x=>morseCode(x))
-    c.toString()
 
+  val reversedMorse = morseCode.map(x => x.swap) //reversed key value to value key
 
+  def letterToMorse(letterString: String): String = {
+    val uppercaseList = letterString.toUpperCase.toList
+    val morseCodeList = uppercaseList.map(x=>morseCode(x))
+    morseCodeList.mkString(" ") //make string
   }
 
-  println(letterToMorse("shivani"))
+  def morseToLetter(morseString: String): String = {
+    val morseList = morseString.trim.split(" ").toList //added trim in case of unnecessary spaces in front of and at end of word
+    val letterList = morseList.map(x => reversedMorse(x))
+    letterList.mkString //make string
+  }
+
+  println("shivani in morse code is " + letterToMorse("shivani"))
+
+  println("morse to words is " + morseToLetter("    - .... .. ... / .. ... / ... -.-. .- .-.. .-   "))
+//morse to words is THIS IS SCALA
+ //when applying use try catch in main file
 }
