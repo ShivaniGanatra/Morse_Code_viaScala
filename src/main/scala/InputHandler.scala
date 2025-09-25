f285424e4f801089421f186dd3b0c063219b98cd
ShivaniGanatra <shivaniganatra@live.com>
2025-09-25 16:08:52 +0100

    Figured out way to validate morse

--- src/main/scala/InputHandler.scala
+++ src/main/scala/InputHandler.scala
@@ -6,29 +6,35 @@ class InputHandler {
   val beginningSentence = "Hi please enter your name : "
 
   final def helloPerson(personName:String): Unit = {
-    println(s"Hello $personName! Can you please type A or a if you want to translate a word/sentence to morse. Type B or b if you \n" +
-      s"want to translate morse to a word/sentence: ")
+    println(s"Hello $personName! Can you please type A or a if you want to translate a word/sentence to morse." +
+      s" \nType B or b if you want to translate morse to a word/sentence: ")
   }
 
   @tailrec
-  final def chooseOne: String = {
+  final def chooseAorB: String = {
     val line = readLine()
     line match {
       case "A" => line
       case "a" => line
       case "B" => line
       case "b" => line
-      case _ => println("Select A, a, B or b"); chooseOne
+      case _ => println("Select A, a, B or b"); chooseAorB
     }
   }
-  
-  def validateWord():Unit = {
+
+  def validateWord():String = {
     val line = readLine
     if(line.matches("^[a-zA-Z\\s]*$")) return line
-    else println("enter a valid string");validateWord()
+    else println("Please enter a valid word or sentence with only letters and spaces if needed");validateWord()
   }
-  
-  
+
+  def validateMorse(): String = {
+    val line = readLine
+    if (line.contains(".") || (line.contains(".") && line.contains("/")) ) return line
+    else println("Please enter a valid morse code sentence with ... and // if needed"); validateMorse()
+  }
+
+
 }
 
 
--- src/main/scala/MainObject.scala
+++ src/main/scala/MainObject.scala
@@ -4,8 +4,14 @@ object MainObject {
     println(input.beginningSentence)
     val name = scala.io.StdIn.readLine()
     input.helloPerson(name)
-    val choiceTypes = input.chooseOne
+    val choiceTypes = input.chooseAorB
     println("choice types " + choiceTypes)
-    val hey = input.validateWord()
+    //a is word to morse
+    //b is morse to word
+    val result = if(choiceTypes == "A" || choiceTypes == "a") input.validateWord()
+    else input.validateMorse()
+    println("valid result " + result)
+
+
   }
 }
